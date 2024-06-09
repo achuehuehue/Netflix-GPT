45ca77b7c50f47beaa94f736b153bf2c7d4ec317
achuehuehue <akanshachouhan2004.06@gmail.com>
2024-06-10 04:25:25 +0530

    GPT feature added

--- src/components/GptMovie.js
+++ src/components/GptMovie.js
@@ -1,9 +1,23 @@
 import React from 'react'
+import { useSelector } from 'react-redux';
+import MovieList from './MovieList';
 
 const GptMovie = () => {
+    const {movieNames,movieResults}=useSelector(store=>store.gpt);
+    if(!movieNames){
+        return null;
+    }
+
+
+
   return (
-    <div>GptMovie</div>
+    <div className="p-4 m-4 bg-black bg-opacity-80 text-white">
+     <div>
+        {movieNames.map((movieNames,index)=>
+        <MovieList key={movieNames} title={movieNames} movies={movieResults[index]} ></MovieList>)}
+     </div>
+    </div>
   )
 }
 
-export default GptMovie
+export default GptMovie;
--- src/components/GptSearchBar.js
+++ src/components/GptSearchBar.js
@@ -1,19 +1,67 @@
 import React, { useRef } from 'react'
 import lang from '../utils/languageConstants'
-import { useSelector } from 'react-redux'
-import openai from '../utils/openAI';
+import { useDispatch, useSelector } from 'react-redux'
+import groq from '../utils/openAI';
+import Error from './Error';
+import { API_OPTIONS } from '../utils/constants';
+import { addGptMovieResults } from '../utils/gptSlice';
+
 
 const GptSearchBar = () => {
     const searchText=useRef(null);
     const langKey=useSelector(store=>store.lang.lang);
+    const dispatch=useDispatch(null);
+
+    const searchMovie=async(movie)=>{
+        const data =await fetch('https://api.themoviedb.org/3/search/movie?query=' +movie.name+ '&include_adult=false&language=en-US&page=1', API_OPTIONS);
+        const json=await data.json();
+        return json.results;
+    }
+
     const handleGptSearchClick= async ()=>{
         // console.log(searchText.current.value);
-        const gptQuery="Act as a Movie Recommendation System and suggest some movies for the query:" + searchText.current.value+ ".Only give me name of 5 movies in a format like the example shown ahead. Example:1.Gadar 2.Sholay 3.Rockstar 4.Golmaal 5.DDLJ" ;
-        const gptResults= await openai.chat.completions.create({
-            messages: [{ role: 'user', content: gptQuery }],
-            model: 'gpt-3.5-turbo',
+        const gptQuery=`Act as a Movie Recommendation System and
+         suggest some movies containing `
+          + searchText.current.value+ `The movie list should be in form of json object.
+           The example is given below.
+            [
+            {
+                "name": "Movie1",
+                "genre": "genre1",
+                "year": "XXXX"
+              },
+            ]` ;
+        const result=await groq.chat.completions.create({
+            messages: [
+              {
+                role: "user",
+                content: gptQuery,
+              },
+            ],
+            model: "llama3-8b-8192",
+            temperature:0.0,
           });
-        console.log(gptResults.choices);
+          if(!result){
+            <Error></Error>
+          }
+          const text=result?.choices[0]?.message?.content;
+          const extractStringInsideCodeBlock = (text) => {
+            const regex = /```([\s\S]*?)```/;
+            const match = text.match(regex);
+            if (match && match[1]) {
+              return match[1].trim();
+            }
+            return null;
+          };
+          
+          const extractedJson = JSON.parse(extractStringInsideCodeBlock(text));
+        //   console.log(extractedJson);
+          const extractedJsonName=extractedJson.map(x=>x?.name);
+        //   console.log(extractedJsonName);
+          const promiseArray=extractedJson.map(movie=>searchMovie(movie)); //[promise,promise,promise.....]
+          const tmdbResult=await Promise.all(promiseArray);
+          console.log(tmdbResult);
+          dispatch(addGptMovieResults({movieNames:extractedJsonName,movieResults:tmdbResult}));
     }
 
 
